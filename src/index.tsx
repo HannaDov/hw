3e0d9ab44e23fdb0c1a96df4d97262615d536e3e
HannaDov <dovgiallohanna@gmail.com>
2023-02-05 01:16:07 +0300

    hw 10

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
 
 import {Provider} from 'react-redux'
+import store from "./s2-homeworks/hw10/bll/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
 
+        <Provider store={store}>
             <App/>
+        </Provider>
 
     </React.StrictMode>
 )
---
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -0,0 +1,26 @@
+const initState = {
+    isLoading: false,
+}
+export type LoadingStateType={
+    isLoading: boolean
+}
+export const loadingReducer = (state = initState, action: LoadingActionType):LoadingStateType=> { // fix any
+    switch (action.type) {
+        // пишет студент  // need to fix
+        case "CHANGE_LOADING":{
+            return {isLoading:action.isLoading}
+        }
+        default:
+            return state
+    }
+}
+
+type LoadingActionType = {
+    type: 'CHANGE_LOADING'
+    isLoading: boolean
+}
+
+export const loadingAC = (isLoading: boolean): LoadingActionType => ({
+    type: 'CHANGE_LOADING',
+    isLoading,
+})
